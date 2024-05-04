6433462197b19841d31998e8f66aa7a4ebc19999
Sungil <102838221+sude58@users.noreply.github.com>
2024-05-04 08:03:07 -0500

    Finished third chapter exercise

---
+++ book/Variables/exercise2.py
@@ -0,0 +1,10 @@
+index #idiomatic
+CatName #non-idiomatic: following Pascalcase while should following snake case
+lazy_dog # idiomatic
+quick_Fox # non-idiomatic: Capitalizing start of the second word when shouldn't
+1stCharacter # illegal: start with digit
+operand2 # idiomatic
+BIG_NUMBER # non-idiomatic: following class naming convention instead of variable naming convention
+π # non-idiomatic: not using standard ASCII character
+
+# Same as exercise 1 since function naming convention follows same convention as variable.
---
+++ book/Variables/exercise4.py
@@ -0,0 +1,8 @@
+# Class naming convention
+index # non-idiomatic: Should start with capital letter
+CatName # idiomatic
+Lazy_Dog # non-idiomatic: shouldn't use underscore
+1ST # illegal: shouldn't start with number
+operand2 # non-idiomatic: shoud start with capital letter
+BigNumber3 # idiomatic
+Πi # non-idiomatic: using unicode character
