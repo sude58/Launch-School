27695c788eff4c55b9d6d12179e0a4bf24666d49
Sungil <102838221+sude58@users.noreply.github.com>
2024-05-16 00:00:37 +0900

    Finished ninth basic exercises

---
+++ exercise/Dictionaries/WhatColor.py
@@ -0,0 +1,8 @@
+car = {
+    'type':  'sedan',
+    'color': 'blue',
+    'year':  2003,
+}
+
+color = car['color']
+print(color)
---
+++ exercise/Dictionaries/WhichCollection.py
@@ -0,0 +1,10 @@
+car = {
+    'type':  'sedan',
+    'color': 'blue',
+    'year':  2003,
+}
+
+list_car = [['type', 'sedan'],
+             ['color', 'blue'],
+               ['year', 2003]
+               ]
